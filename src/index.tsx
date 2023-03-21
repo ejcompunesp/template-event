ee348dbde1208f7ea2d71051c33e38d3f7cc9467
Keusan <sara.albuquerque1503@gmail.com>
2023-03-21 10:57:19 -0300

    configuração de navegação

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
+import React from "react"
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'App'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
+import Root from "./routes/root"
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+    },
+]);
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>)
